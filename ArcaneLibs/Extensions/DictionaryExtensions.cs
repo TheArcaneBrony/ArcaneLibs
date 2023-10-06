af68b2a84885776219aa8fb0e24cd99558a559db
TheArcaneBrony <myrainbowdash949@gmail.com>
2023-10-06 23:43:30 +0200

    Slight reformat

--- ArcaneLibs/Extensions/DictionaryExtensions.cs
+++ ArcaneLibs/Extensions/DictionaryExtensions.cs
@@ -26,29 +26,31 @@ public static class DictionaryExtensions {
         if (dict.TryGetValue(key, out var value)) return value;
 
         value = valueFactory(key);
-        lock(dict)
+        lock (dict)
             dict.TryAdd(key, value);
         return value;
     }
-    public static async Task<Y> GetOrCreateAsync<X, Y>(this IDictionary<X, Y> dict, X key, Func<X, Task<Y>> valueFactory) {
-        if (dict.TryGetValue(key, out var value)) return value;
+
+    public static async Task<Y> GetOrCreateAsync<X, Y>(this IDictionary<X, Y> dict, X key, Func<X, Task<Y>> valueFactory, SemaphoreSlim? semaphore = null) {
+        if (semaphore is not null) await semaphore.WaitAsync();
+        if (dict.TryGetValue(key, out var value)) {
+            if (semaphore is not null) semaphore.Release();
+            return value;
+        }
 
         value = await valueFactory(key);
         dict.Add(key, value);
+        if (semaphore is not null) semaphore.Release();
         return value;
     }
 
-
-    public static bool StartsWith<T>(this IEnumerable<T> list, IEnumerable<T> prefix)
-    {
+    public static bool StartsWith<T>(this IEnumerable<T> list, IEnumerable<T> prefix) {
         return prefix.SequenceEqual(list.Take(prefix.Count()));
     }
 
-    public static void HexDump(this IEnumerable<byte> bytes, int width = 32)
-    {
+    public static void HexDump(this IEnumerable<byte> bytes, int width = 32) {
         var data = new Queue<(string hex, char utf8)>(bytes.ToArray().Select(x => ($"{x:X2}", (char)x)).ToArray());
-        while (data.Count > 0)
-        {
+        while (data.Count > 0) {
             var line = data.Dequeue(Math.Min(width, data.Count)).ToArray();
             Console.WriteLine(
                 string.Join(" ", line.Select(x => x.hex)).PadRight(width * 3)
@@ -69,10 +71,8 @@ public static class DictionaryExtensions {
     public static string AsHexString(this IEnumerable<byte> bytes) => string.Join(' ', bytes.Select(x => $"{x:X2}"));
     public static string AsString(this IEnumerable<byte> bytes) => Encoding.UTF8.GetString(bytes.ToArray());
 
-
     //zlib decompress
-    public static byte[] ZlibDecompress(this IEnumerable<byte> bytes)
-    {
+    public static byte[] ZlibDecompress(this IEnumerable<byte> bytes) {
         var inStream = new MemoryStream(bytes.ToArray());
         using ZLibStream stream = new ZLibStream(inStream, CompressionMode.Decompress);
         using var result = new MemoryStream();
