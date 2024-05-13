6f6b8e436eaa85eefa96dbf4d92b7aa21fb07ab8
Rory& <root@rory.gay>
2024-05-13 23:52:42 +0200

    Expiring semaphore cache

--- ArcaneLibs/Collections/SemaphoreCache.cs
+++ ArcaneLibs/Collections/SemaphoreCache.cs
@@ -4,50 +4,41 @@ namespace ArcaneLibs.Collections;
 
 public class SemaphoreCache<T> where T : class {
     
-    private readonly ConcurrentDictionary<string, T> _values = new();
-    private readonly ConcurrentDictionary<string, SemaphoreSlim> _semaphores = new();
+    internal readonly ConcurrentDictionary<string, T> Values = new();
+    internal readonly ConcurrentDictionary<string, SemaphoreSlim> Semaphores = new();
     
     public async Task<T> GetOrAdd(string key, Func<Task<T>> factory) {
         ArgumentNullException.ThrowIfNull(key);
-        _semaphores.TryAdd(key, new SemaphoreSlim(1, 1));
-        await _semaphores[key].WaitAsync();
-        if (_values.TryGetValue(key, out var value)) {
-            _semaphores[key].Release();
+        Semaphores.TryAdd(key, new SemaphoreSlim(1, 1));
+        await Semaphores[key].WaitAsync();
+        if (Values.TryGetValue(key, out var value)) {
+            Semaphores[key].Release();
             return value;
         }
 
         var val = await factory();
-        _values.TryAdd(key, val);
-        _semaphores[key].Release();
+        Values.TryAdd(key, val);
+        Semaphores[key].Release();
         return val;
     }
 }
 
-public class ExiringSemaphoreCache<T> where T : class {
-    
-    private readonly ConcurrentDictionary<string, T> _values = new();
-    private readonly ConcurrentDictionary<string, SemaphoreSlim> _semaphores = new();
+public class ExpiringSemaphoreCache<T> : SemaphoreCache<T> where T : class  {
+    private readonly ConcurrentDictionary<string, DateTime> _expiry = new();
     
     public async Task<T> GetOrAdd(string key, Func<Task<T>> factory, TimeSpan expiry) {
         ArgumentNullException.ThrowIfNull(key);
-        _semaphores.TryAdd(key, new SemaphoreSlim(1, 1));
-        await _semaphores[key].WaitAsync();
-        if (_values.TryGetValue(key, out var value)) {
-            _semaphores[key].Release();
+        Semaphores.TryAdd(key, new SemaphoreSlim(1, 1));
+        await Semaphores[key].WaitAsync();
+        if (Values.TryGetValue(key, out var value) && _expiry.TryGetValue(key, out var exp) && exp > DateTime.Now) {
+            Semaphores[key].Release();
             return value;
         }
 
         var val = await factory();
-        _values.TryAdd(key, val);
-        _semaphores[key].Release();
-        
-#pragma warning disable CS4014 // We intentionally do not await this task - this handles cache expiry
-        Task.Delay(expiry).ContinueWith(__ => {
-            _values.TryRemove(key, out var _);
-            _semaphores.TryRemove(key, out var _);
-        });
-#pragma warning restore CS4014
-        
+        Values.TryAdd(key, val);
+        _expiry.TryAdd(key, DateTime.Now + expiry);
+        Semaphores[key].Release();
         return val;
     }
 }
