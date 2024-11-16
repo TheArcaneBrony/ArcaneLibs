8a9bae10b5e18341906b0f86f0d3831a612b5cb2
Rory& <root@rory.gay>
2024-11-16 01:18:22 +0100

    Update to .NET 9

--- ArcaneLibs/Extensions/CollectionExtensions.cs
+++ ArcaneLibs/Extensions/CollectionExtensions.cs
@@ -8,6 +8,7 @@ public static class CollectionExtensions {
     }
 
     //return task results async without preserving order
+    [Obsolete("Replaced by Task.WhenEach in .NET 9")]
     public static async IAsyncEnumerable<T> ToAsyncEnumerable<T>(this IEnumerable<Task<T>> tasks, bool skipExceptions = false) {
         var taskList = tasks.ToList();
         while (taskList.Count > 0) {
