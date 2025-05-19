58a531f72aaae7d44420fcaa7feb5f494c069971
Rory& <root@rory.gay>
2025-05-19 15:16:27 +0200

    Use builtin element focus in FancyTextBox, some cleanup

--- ArcaneLibs.Blazor.Components/FancyTextBox.razor.js
+++
@@ -1,9 +0,0 @@
-function BlazorFocusElement(element) {
-    if (element == null) return;
-    if (element instanceof HTMLElement) {
-        console.log(element);
-        element.focus();
-    } else if (element.hasOwnProperty("__internalId")) {
-        console.log("Element is not an HTMLElement", element);
-    }
-}
