dc1442ea846f4dfdba920ac44e8aa83130901cd2
Rory& <root@rory.gay>
2025-11-02 22:43:42 +0100

    Fix nupkg stuff

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,10 @@
-export DATE=`git log -1 --format="%at" | xargs -I{} date -d @{} '+%Y%m%d-%H%M%S'`
-#export DATE=`date -u '+%Y%m%d-%H%M%S'`
-export REV=`git rev-parse --short HEAD`
+#DATE=`date -u '+%Y%m%d-%H%M%S'`
+DATE=`git log -1 --format="%at" | xargs -I{} date -d @{} '+%Y%m%d-%H%M%S'`
+REV=`git rev-parse --short HEAD`
+BASEDIR="$PWD"
 echo "preview.$DATE+$REV" > version.txt
 git add version.txt
-rm ./result*
+rm ./result* *.nupkg
 
 for p in `nix flake show --json | jq '.packages."x86_64-linux" | keys[]' -r`
 do
@@ -11,5 +12,15 @@ do
 done
 wait
 
-#set +x
-dotnet nuget push result*/share/nuget/source/*/*/*.nupkg -k ${NUGET_KEY} --source https://api.nuget.org/v3/index.json --skip-duplicate
+
+
+for p in result*/share/nuget/packages/*/*/.unpacked
+do
+    PNAME=$(basename `realpath "${p}/../.."`)
+    echo $PNAME:
+    cd "${p}"
+    zip -db -ds 32k -9 -r "${BASEDIR}/${PNAME//./-}.nupkg" *
+    cd -
+done
+
+dotnet nuget push *.nupkg -k ${NUGET_KEY} --source https://api.nuget.org/v3/index.json --skip-duplicate
