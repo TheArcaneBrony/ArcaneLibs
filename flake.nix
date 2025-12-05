491a3c65481c4e5a41b4435106e3dc8ba81efdc3
Rory& <root@rory.gay>
2025-12-05 12:39:31 +0100

    Clean flake, more outputs

--- flake.nix
+++ flake.nix
@@ -10,26 +10,35 @@
     }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      rVersion = let
-        rev = self.sourceInfo.shortRev or self.sourceInfo.dirtyShortRev;
-        date = builtins.substring 0 8 self.sourceInfo.lastModifiedDate;
-        time = builtins.substring 8 6 self.sourceInfo.lastModifiedDate;
-      in "preview.${date}-${time}+${rev}";
+      rVersion =
+        let
+          rev = self.sourceInfo.shortRev or self.sourceInfo.dirtyShortRev;
+          date = builtins.substring 0 8 self.sourceInfo.lastModifiedDate;
+          time = builtins.substring 8 6 self.sourceInfo.lastModifiedDate;
+        in
+        "preview.${date}-${time}+${rev}";
       makeNupkg =
-        {name, nugetDeps ? null, projectReferences ? []}:
+        {
+          name,
+          nugetDeps ? null,
+          projectReferences ? [ ],
+        }:
         pkgs.buildDotnetModule rec {
           inherit projectReferences nugetDeps;
 
           pname = "${name}";
           version = "1.0.0-" + rVersion;
-          dotnetPackFlags = [ "--include-symbols" "--include-source" "--version-suffix ${rVersion}" ];
+          dotnetPackFlags = [
+            "--include-symbols"
+            "--include-source"
+            "--version-suffix ${rVersion}"
+          ];
           dotnet-sdk = pkgs.dotnet-sdk_10;
           dotnet-runtime = pkgs.dotnet-aspnetcore_10;
-          src = ./.;
+          src = pkgs.lib.cleanSource ./.;
           projectFile = [
             "${name}/${name}.csproj"
           ];
-#          nugetDeps = if (depsFile != "") then (./. + depsFile) else null;
           packNupkg = true;
           meta = with pkgs.lib; {
             description = "ArcaneLibs .NET Libraries";
@@ -45,14 +54,45 @@
     {
       packages.x86_64-linux = {
         ArcaneLibs = makeNupkg { name = "ArcaneLibs"; };
-        ArcaneLibs-Blazor-Components = makeNupkg { name = "ArcaneLibs.Blazor.Components"; nugetDeps = ./ArcaneLibs.Blazor.Components/deps.json; };
+        ArcaneLibs-Blazor-Components = makeNupkg {
+          name = "ArcaneLibs.Blazor.Components";
+          nugetDeps = ./ArcaneLibs.Blazor.Components/deps.json;
+        };
         ArcaneLibs-Legacy = makeNupkg { name = "ArcaneLibs.Legacy"; };
         ArcaneLibs-Logging = makeNupkg { name = "ArcaneLibs.Logging"; };
-        ArcaneLibs-StringNormalisation = makeNupkg { name = "ArcaneLibs.StringNormalisation"; nugetDeps = ./ArcaneLibs.StringNormalisation/deps.json; };
+        ArcaneLibs-StringNormalisation = makeNupkg {
+          name = "ArcaneLibs.StringNormalisation";
+          nugetDeps = ./ArcaneLibs.StringNormalisation/deps.json;
+        };
         ArcaneLibs-Timings = makeNupkg { name = "ArcaneLibs.Timings"; };
       };
       checks = pkgs.lib.attrsets.unionOfDisjoint {
         # Actual checks
       } self.packages;
+      nupkgs.x86_64-linux = pkgs.lib.mapAttrs (
+        name: pkg:
+        (
+          with pkgs;
+          pkgs.runCommand (pkg.pname + "-" + pkg.version + ".nupkg") { } ''
+            echo 'Creating zip archive for ${name}'
+            set -x
+            cd "${pkg.out}/share/nuget/packages/${lib.toLower pkg.pname}/${pkg.version}"
+            ls -la
+            # NuGet doesn't care about compression flags
+            ${lib.getExe pkgs.zip} -db -ds 32k -9 -r "$out" *
+            set +x
+          ''
+        )
+      ) self.packages.x86_64-linux;
+      nugetArtifactDir.x86_64-linux =
+        let
+          outPaths = pkgs.lib.mapAttrsToList (name: pkg: pkg.out) self.nupkgs.x86_64-linux;
+        in
+        pkgs.runCommand "nuget-artifacts" { } ''
+          mkdir -p $out
+          for path in ${pkgs.lib.concatStringsSep " " outPaths}; do
+            ln -vs ''\${path} $out/
+          done
+        '';
     };
 }
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,20 +1,27 @@
 BASEDIR="$PWD"
 rm ./result* *.nupkg
 
-for p in `nix flake show --json | jq '.packages."x86_64-linux" | keys[]' -r`
-do
-    nix build .\#${p} -j4 -L --out-link ./result-${p//-/\.}
-done
-wait
+#for p in `nix flake show --json | jq '.packages."x86_64-linux" | keys[]' -r`
+#do
+#    nix build .\#${p} -j4 -L --out-link ./result-${p//-/\.}
+#done
+#wait
+#
+#for p in result*/share/nuget/packages/*/*/.unpacked
+#do
+#    PNAME=$(basename `realpath "${p}/../.."`)
+#    PRNAME=$(basename $(cd "${p}/../../../../../.." && echo $PWD))
+#    echo $PNAME: $PRNAME
+#    cd "${p}" || continue
+#    zip -db -ds 32k -9 -r "${BASEDIR}/${PNAME//./-}.nupkg" *
+#    cd -
+#    dotnet nuget push *.nupkg -k ${NUGET_KEY} --source https://api.nuget.org/v3/index.json --skip-duplicate
+#    rm -rfv "${PRNAME}" "${PNAME//./-}.nupkg"
+#done
 
-for p in result*/share/nuget/packages/*/*/.unpacked
-do
-    PNAME=$(basename `realpath "${p}/../.."`)
-    PRNAME=$(basename $(cd "${p}/../../../../../.." && echo $PWD))
-    echo $PNAME: $PRNAME
-    cd "${p}" || continue
-    zip -db -ds 32k -9 -r "${BASEDIR}/${PNAME//./-}.nupkg" *
-    cd -
-    dotnet nuget push *.nupkg -k ${NUGET_KEY} --source https://api.nuget.org/v3/index.json --skip-duplicate
-    rm -rfv "${PRNAME}" "${PNAME//./-}.nupkg"
+nom build .\#nugetArtifactDir.x86_64-linux -j4 -L
+for p in ./result/*.nupkg; do
+    echo "Pushing $p to NuGet..."
+    dotnet nuget push "$p" -k ${NUGET_KEY} --source https://api.nuget.org/v3/index.json --skip-duplicate
 done
+rm -rfv ./result
