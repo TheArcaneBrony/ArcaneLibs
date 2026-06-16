03f2c914b6ae617af68ec7c8ac80832881ddf918
Rory& <root@rory.gay>
2026-06-16 23:32:57 +0200

    Tests for friendly name attribute

--- ArcaneLibs.Tests/UnitTest1.cs
+++ ArcaneLibs.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using ArcaneLibs.Attributes;
 
 namespace ArcaneLibs.Tests;
 
@@ -9,5 +10,28 @@ public class UnitTest1 {
         Assert.Equal(6, Util.Max(2, 4, 6));
         Assert.Equal(2d, Util.Min(2d, 4d, 6d));
         Assert.Equal(6d, Util.Max(2d, 4d, 6d));
+    }
+
+    [Fact]
+    public void TestFriendlyName() {
+        Assert.Equal("Class", typeof(FriendlyName).GetFriendlyName());
+        Assert.Equal("Property", typeof(FriendlyName).GetProperty("Property")?.GetFriendlyName());
+        Assert.Equal("Field", typeof(FriendlyName).GetField("Field")?.GetFriendlyName());
+        Assert.Equal("Enum", typeof(FriendlyName.FriendlyNameEnum).GetFriendlyName());
+        Assert.Equal("Enum value", typeof(FriendlyName.FriendlyNameEnum).GetField("Meow")?.GetFriendlyName());
+    }
+}
+
+[FriendlyName(Name = "Class")]
+internal class FriendlyName {
+    [FriendlyName(Name = "Property")]
+    public object? Property { get; set; }
+
+    [FriendlyName(Name = "Field")] public object? Field;
+
+    [FriendlyName(Name = "Enum")]
+    internal enum FriendlyNameEnum {
+        [FriendlyName(Name = "Enum value")]
+        Meow
     }
 }
